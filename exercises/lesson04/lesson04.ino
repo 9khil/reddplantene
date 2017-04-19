aeb1d254eee180b94294e93ffbce411ad0db9180
Nikhil André Luthra <nikhilandreluthra@LuthraMBPR.home>
2017-04-19 14:29:11 +0200

    fixed lesson04 with got reverted to something really old

--- exercises/lesson04/lesson04.ino
+++ exercises/lesson04/lesson04.ino
@@ -1,18 +1,16 @@
 #include <ESP8266WiFi.h>
-#include <PubSubClient.h>
+#include <Phant.h>
 
 // Fyll ut med SSID og passord
-#define WIFI_SSID "LabbenKurs"
-#define WIFI_PASSWORD "labbenkurs123"
+#define WIFI_SSID ""
+#define WIFI_PASSWORD ""
 
-#define mqtt_server "192.168.2.30"
-//#define mqtt_server "broker.hivemq.com"
-#define topic "labbenkurs/fuktighet"
+// Fyll ut phanthost, publickey og privatekey
+const char PhantHost[] = "phant.labben.org";
+const char PublicKey[] = "";
+const char PrivateKey[] = "";
 
-
-WiFiClient espClient;
-PubSubClient client(espClient);
-
+WiFiClient client;
 const int httpPort = 80;
 
 #define MOISTURE_PIN A0
@@ -33,46 +31,25 @@ void setup() {
   Serial.print("connected: ");
   Serial.println(WiFi.localIP());
 
-  client.setServer(mqtt_server, 1883);
-
 }
-
-void reconnect() {
-  // Loop until we're reconnected
-  while (!client.connected()) {
-    Serial.print("Attempting MQTT connection...");
-    // Attempt to connect
-    // If you do not want to use a username and password, change next line to
-     if (client.connect("ESP8266Client")) {
-      Serial.println("connected");
-    } else {
-      Serial.print("failed, rc=");
-      Serial.print(client.state());
-      Serial.println(" try again in 5 seconds");
-      // Wait 5 seconds before retrying
-      delay(5000);
-    }
-  }
-}
-
-long lastMsg = 0;
 
 void loop() {
+  sensorValue = analogRead(MOISTURE_PIN);
 
-  if (!client.connected()) {
-    reconnect();
-  }
-  client.loop();
-
-  long now = millis();
-  if (now - lastMsg > 1000) {
-    lastMsg = now;
-    sensorValue = analogRead(MOISTURE_PIN);
-    Serial.println(sensorValue);
-    client.publish(topic, String(sensorValue).c_str(), true);
-  }
+  // Oppgave: Deklarer et Phantobjekt
   
 
+  // Oppgave: legg til sensorverdien i Phantobjektet. Husk å bruke samme nøkkel som når du opprettet phant-stream
   
-  delay(5000);
+
+  // Oppgave: koble til Phantserveren. Skriv ut feilmelding om tilkobling mislyktes
+  
+  
+
+  // Oppgave: Post data til Phant serveren
+
+
+
+
+  delay(15000);
 }
