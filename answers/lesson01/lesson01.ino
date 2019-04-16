8fb591d6f989eca7bc3ad16a360b939897fed7d1
AsplemEirik <eirik.asplem@bouvet.no>
2019-04-16 07:27:45 +0200

    Added english comments in exercise 1

--- answers/lesson01/lesson01.ino
+++ answers/lesson01/lesson01.ino
@@ -4,17 +4,21 @@
  * @web: bouvet.no / nikhil.luthra.no
  */
 void setup() {
+  // Initialize serial port with 11520 bps
   // Initialiser serieport på 115200 bps
   Serial.begin(115200);
 
+  // Print a message to serial port
   // Skriv ut melding til serieport
   Serial.println("Hello serial port");
 }
 
 void loop() {
+  // Write a mesage to serial port
   // Skriv ut melding til serieport
   Serial.println("Hello loop");
 
+  // Wait for 2 seconds
   // Vent 2 sekunder
   delay(2000);
 }
--- exercises/lesson01/lesson01.ino
+++ exercises/lesson01/lesson01.ino
@@ -4,13 +4,17 @@
  * @web: bouvet.no / nikhil.luthra.no
  */
 void setup() {
+  // Excercise: Initialize serial port with 115200 bps
   // Oppgave: Initialiser serieport på 115200 bps
 
+  // Excercise: Print a message to serial port
   // Oppgave: Skriv ut melding til serieport
 }
 
 void loop() {
+  // Exercise: Print a message to serial port
   // Oppgave: Skriv ut melding til serieport
 
+  // Exercise: Wait for 2 seconds
   // Oppgave: Vent 2 sekunder
 }
